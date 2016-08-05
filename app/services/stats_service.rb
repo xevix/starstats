19ac4176223b0709cd620fae89655342cb4bbe62
Alejandro Wainzinger <aikawarazuni@gmail.com>
2016-08-04 23:59:47 -0700

    Load all pages of results from API.

--- app/services/stats_service.rb
+++ app/services/stats_service.rb
@@ -4,7 +4,8 @@ class StatsService
   STARRED_KEY = "my:starred"
   GITHUB_USER = ENV["github_user"]
 
-  # [[year, [month, count], [month, count]]]
+  # TODO: reorganize results so view needs no logic for displaying
+  # [year, [[month, count], [month, count]]]
   def starred_per_month
     starred = fetch_starred
     starred_by_month = {}
@@ -32,16 +33,28 @@ class StatsService
         end
 
     # If results are missing, cache them
-    # TODO: Do the full list, not just the first page
+    # TODO: Proper check from the API periodically for new data
     if latest_starred_redis.nil?
       # Grab the latest from the API
-      starred_api = $octokit.starred(GITHUB_USER, accept: 'application/vnd.github.v3.star+json', sort: "created", direction: "desc")
-      starred_api.each do |starred_entry|
-        $redis.rpush STARRED_KEY, starred_entry.to_attrs.to_json
+      $octokit.starred(GITHUB_USER, accept: 'application/vnd.github.v3.star+json', sort: "created", direction: "desc", per_page: 100)
+
+      # Store response for pagination links
+      latest_api_response = $octokit.last_response
+
+      # Loop until there's no next page
+      while true do
+        latest_api_response.data.each do |starred_entry|
+          $redis.rpush STARRED_KEY, starred_entry.to_attrs.to_json
+        end
+
+        break unless latest_api_response.rels[:next]
+
+        latest_api_response = latest_api_response.rels[:next].call({sort: "created", direction: "desc", per_page: 100}, {method: :get, headers: {accept: 'application/vnd.github.v3.star+json'}})
       end
     end
 
     # Fetch from cache and return
     $redis.lrange(STARRED_KEY, 0, -1).collect { |e| JSON.parse(e) }
   end
+
 end
